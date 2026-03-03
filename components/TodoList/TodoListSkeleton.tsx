af96b04cb3e3c39a6f57c54efb5d124d13d8874a
Andy Pataki <anpataki@gmail.com>
2026-03-03 16:22:30 +0100

    Improved TodoListSkeleton

--- components/TodoList/TodoListSkeleton.tsx
+++ components/TodoList/TodoListSkeleton.tsx
@@ -2,12 +2,26 @@ import { Skeleton } from "../ui/skeleton";
 
 function TodoListSkeleton() {
 	return (
-		<div className="space-y-6 px-3 py-5">
+		<div className="space-y-5 px-2 py-5">
 			<Skeleton className="mt-2 h-5 w-[75%] rounded-md" />
 			<Skeleton className="mt-2 h-5 w-[75%] rounded-md md:hidden" />
-			<Skeleton className="mt-5 h-10 w-full rounded-md" />
-			<Skeleton className="h-10 w-full rounded-md" />
-			<div className="flex justify-center">
+			<div className="grid grid-cols-[auto_1fr_auto] items-center space-x-2 pt-1">
+				<Skeleton className="h-4 w-4 rounded-xs" />
+				<div>
+					<Skeleton className="h-4 w-[75%] rounded-md" />
+					<Skeleton className="mt-2 h-3.5 w-[50%] rounded-md" />
+				</div>
+				<Skeleton className="h-8 w-19 rounded-md" />
+			</div>
+			<div className="grid grid-cols-[auto_1fr_auto] items-center space-x-2 pt-1">
+				<Skeleton className="h-4 w-4 rounded-xs" />
+				<div>
+					<Skeleton className="h-4 w-[75%] rounded-md" />
+					<Skeleton className="mt-2 h-3.5 w-[50%] rounded-md" />
+				</div>
+				<Skeleton className="h-8 w-19 rounded-md" />
+			</div>
+			<div className="flex justify-center pt-1 pb-1.5">
 				<Skeleton className="mt-3 h-5 w-[40%] rounded-md" />
 			</div>
 		</div>
